cccc9464c87a13ac75019ac3fe08d3301016bba9
naiaden <louis@naiaden.nl>
2017-12-07 23:51:31 +0100

    Improved scales and plots

--- data/concat/hexbin.py
+++ data/concat/hexbin.py
@@ -1,10 +1,18 @@
 
+#slm_in = 'compare-srilm-slm.c.slm_fulluniform.probs'
+slm_in = '../utterance/compare-srilm-slm.slm_fulluniform.probs'
+
+#srilm_in = 'compare-srilm-slm.c.srilm-out1'
+srilm_in = '../utterance/compare-srilm-slm.srilm-out1'
+
+#c_or_u = 'Concat'
+c_or_u = 'Utterance'
 
 
 # slm
 slm_lines = []
 current_line = []
-with open('compare-srilm-slm.c.slm_fulluniform.probs', 'r', encoding='iso-8859-1') as f:
+with open(slm_in, 'r', encoding='iso-8859-1') as f:
     for line in f:
         if "<s> <s> <s>" in line:
             slm_lines.append(current_line)
@@ -60,7 +68,7 @@ rest_slm_lines = sum(rest_slm_lines, [])
 # srilm
 srilm_lines = []
 current_line = []
-with open('compare-srilm-slm.c.srilm-out1', 'r', encoding='iso-8859-1') as f:
+with open(srilm_in, 'r', encoding='iso-8859-1') as f:
     for line in f:
         if line.startswith("\tp") and not "<s> | <s>" in line:
             if "| <s> ...)" in line:
@@ -125,34 +133,36 @@ import matplotlib.pyplot as plt
 
 #plt.savefig('hallo.pgf')
 
-f, axarr = plt.subplots(2, 5)
-axarr[0, 0].hexbin(numpy.asarray(first_slm_lines), numpy.asarray(first_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[0, 0].set_title('Axis [0,0]')
+f, ((ax11, ax12, ax13, ax14, ax15), (ax21, ax22, ax23, ax24, ax25)) = plt.subplots(2, 5, sharex=True, sharey=True)
+f.suptitle(c_or_u + ": slm op x, srilm op y")
+hb11 = ax11.hexbin(numpy.asarray(first_slm_lines), numpy.asarray(first_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax11.set_title('1e woord')
 
-axarr[0, 1].hexbin(numpy.asarray(second_slm_lines), numpy.asarray(second_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[0, 1].set_title('Axis [0,1]')
+hb12 = ax12.hexbin(numpy.asarray(second_slm_lines), numpy.asarray(second_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax12.set_title('2e woord')
 
-axarr[0, 2].hexbin(numpy.asarray(third_slm_lines), numpy.asarray(third_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[0, 2].set_title('Axis [0,2]')
+hb13 = ax13.hexbin(numpy.asarray(third_slm_lines), numpy.asarray(third_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax13.set_title('3e woord')
 
-axarr[0, 3].hexbin(numpy.asarray(fourth_slm_lines), numpy.asarray(fourth_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[0, 3].set_title('Axis [0,3]')
+hb14 = ax14.hexbin(numpy.asarray(fourth_slm_lines), numpy.asarray(fourth_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax14.set_title('4e woord')
 
-axarr[0, 4].hexbin(numpy.asarray(rest_slm_lines), numpy.asarray(rest_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[0, 4].set_title('Axis [0,4]')
+hb15 = ax15.hexbin(numpy.asarray(rest_slm_lines), numpy.asarray(rest_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax15.set_title('4e tem -3e woord')
 
-axarr[1, 0].hexbin(numpy.asarray(last_slm_lines), numpy.asarray(last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[1, 0].set_title('Axis [1,0]')
+hb21 = ax21.hexbin(numpy.asarray(last_slm_lines), numpy.asarray(last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax21.set_title('-3e woord')
 
-axarr[1, 1].hexbin(numpy.asarray(second_to_last_slm_lines), numpy.asarray(second_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[1, 1].set_title('Axis [1,1]')
+hb22 = ax22.hexbin(numpy.asarray(second_to_last_slm_lines), numpy.asarray(second_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax22.set_title('-2e woord')
 
-axarr[1, 2].hexbin(numpy.asarray(third_to_last_slm_lines), numpy.asarray(third_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[1, 2].set_title('Axis [1,2]')
+hb23 = ax23.hexbin(numpy.asarray(third_to_last_slm_lines), numpy.asarray(third_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax23.set_title('-1e woord')
 
-axarr[1, 3].hexbin(numpy.asarray(fourth_to_last_slm_lines), numpy.asarray(fourth_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
-axarr[1, 3].set_title('Axis [1,3]')
-# Fine-tune figure; hide x ticks for top plots and y ticks for right plots
-#plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False)
-#plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
+hb24 = ax24.hexbin(numpy.asarray(fourth_to_last_slm_lines), numpy.asarray(fourth_to_last_srilm_lines),gridsize=15, bins='log', xscale='log', yscale='log')
+ax24.set_title('</s>')
+
+cb = f.colorbar(hb24, ax=ax25)
+cb.set_label('log10(N)')
+
 plt.show()
